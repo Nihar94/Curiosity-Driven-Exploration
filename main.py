176b1aa1aa49f2c91ea5cc5e8c9074e050e3bae2
Nihar <tadichetty.nihar@gmail.com>
2018-11-18 00:19:10 -0500

    Weighted gradient update based on episode length

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from A2C import *
 import nel
 import gym
 import pdb
+import time
 
 env = gym.make('NEL-v0')
 
@@ -23,17 +24,35 @@ class Curiosity():
 		self.icm_forward = ForwardModel()
 		self.intrinsic_reward = IntrinsicReward()
 		self.inv_model = InverseModel()
-		self.e = args.e
 		self.N = args.n
-		if(args.load_critic):
-			self.critic_model.load_state_dict(torch.load('/home/nihar/Desktop/DeepRL/Curiosity/models/N_'+str(self.N)+'/critic/'+'critic_model_'+'N_'+str(self.N)+'_episode_'+str(self.e)))
-		if(args.load_actor):
-			self.actor_model.load_state_dict(torch.load('/home/nihar/Desktop/DeepRL/Curiosity/models/N_'+str(self.N)+'/actor/'+'actor_model_'+'N_'+str(self.N)+'_episode_'+str(self.e)))
+		self.epsilon = 0.1
+		if(args.load_models):
+			self.actor_model.load_state_dict(torch.load('./models/actor_model'))
+			self.critic_model.load_state_dict(torch.load('./models/critic_model'))
+			self.features.load_state_dict(torch.load('./models/A2C_features'))
+			self.icm_features.load_state_dict(torch.load('./models/icm_features'))
+			self.inv_model.load_state_dict(torch.load('./models/inv_model'))
+			self.icm_forward.load_state_dict(torch.load('./models/icm_forward'))
+			
 		self.actor_lr = args.actor_lr
 		self.critic_lr = args.critic_lr
+		self.icm_lr = args.icm_lr
 		self.env = env
 		self.num_episodes = args.num_episodes
 
+	def play(self, args):
+		env = gym.make('NEL-render-v0')
+		state = env.reset()
+		e = 0
+		while e<500:
+			env.render()
+			e += 1
+			state = self.features(state)
+			action_probs = self.actor_model(state)
+			action = np.random.randint(env.action_space.n)
+			state, reward, done, info = env.step(action)
+
+	# Generating all episodes on CPU. Loading and Unloading single states is expensive.
 	def generate_episode(self, env):
 		states = []
 		actions = []
@@ -41,6 +60,8 @@ class Curiosity():
 		rewards = []
 		state = env.reset()
 		e = 0
+		self.actor_model = self.actor_model
+		t1 = time.time()
 		while True:
 			e += 1
 			actions_arr = torch.arange(self.env.action_space.n)
@@ -48,7 +69,7 @@ class Curiosity():
 			action_probs = self.actor_model(state)
 			actions_probdist.append(action_probs)
 			p = np.random.random(1)
-			if(np.random.random(1) < 0.3):
+			if(np.random.random(1) < self.epsilon):
 				action = np.random.randint(env.action_space.n)
 			else:
 				action = np.random.choice(actions_arr, p=action_probs.detach().numpy())
@@ -57,27 +78,35 @@ class Curiosity():
 			states.append(state)
 			rewards.append(reward)
 			state = next_state
-			if(e>30000):
-				if(sum(rewards)>0):
-					print('Length of Episode: '+str(len(actions))+', Reward: '+str(sum(rewards)))
-					break
-				else:
-					states = []
-					actions = []
-					rewards = []
+			if(reward>0):
+				t2 = time.time()
+				print('Time taken for generating this episode: ' + str(int(t2-t1)) + ' seconds')
+				break
 		return torch.stack(states), torch.LongTensor(actions), torch.FloatTensor(rewards), torch.stack(actions_probdist)
 
 	def train(self, args, gamma=1.0):
 		actor_optimizer = torch.optim.Adam(self.actor_model.parameters(), lr=self.actor_lr)
 		critic_optimizer = torch.optim.Adam(self.critic_model.parameters(), lr=self.critic_lr)
-		e = self.e
-		while e<self.num_episodes:
+		#list(self.actor_model.parameters()) + list(self.critic_model.parameters()) + 
+		params = list(self.features.parameters()) + list(self.icm_forward.parameters()) + list(self.inv_model.parameters()) + list(self.icm_features.parameters())
+		icm_optimizer = torch.optim.Adam(params, lr=args.icm_lr)
+		e = 0
+		mean_rewards = []
+		while True:
+			print('Episode: '+str(e))
 			e+=1
+			if(e%100):
+				self.epsilon *= 0.95
 			states, actions, rewards, action_probs = self.generate_episode(self.env)
-			
+			self.actor_model = self.actor_model
 			T = len(states)
 			N = self.N
 			R = torch.FloatTensor([0]*T)
+			loss_multiplier = (1/(torch.mean(rewards).item())) # To force large updates on episodes that get rewards late and vice-versa
+			mean_rewards.append(torch.mean(rewards).item())
+			print('Number of actions in episode: '+str(T))
+			print('Mean Reward: '+str(torch.mean(rewards).item()))
+			print('')
 			ICM_loss = 0
 			t2 = 0 #just a tmp for ICMLoss iteration
 			for t in range(T-1, 0, -1):
@@ -85,7 +114,6 @@ class Curiosity():
 				R[t] = (gamma**N)*V_end
 				ICM_loss += self.ICMLoss(states[t2], states[t2+1], action_probs[t2])
 				t2 += 1
-				#pdb.set_trace()
 				tmp = 0
 				for k in range(N):
 					tmp += (gamma**k)*rewards[t+k] if(t+k<T) else 0
@@ -98,18 +126,26 @@ class Curiosity():
 				log_probs[i] = log_prob[i][actions[i]]
 			Vw_St, _ = torch.max(self.critic_model(states),dim=1)
 			scaling_factor = (R.detach() - Vw_St.detach())
+			
 			L_theta = torch.mean(scaling_factor*(-log_probs))
 			L_w = torch.mean((R - Vw_St)**2)
 			actor_optimizer.zero_grad()
 			critic_optimizer.zero_grad()
-			tmp = L_theta + L_w + ICM_loss
+			icm_optimizer.zero_grad()
+
+			tmp = loss_multiplier*(L_theta + L_w + ICM_loss)
 			tmp.backward()
 			actor_optimizer.step()
 			critic_optimizer.step()
-			#if(e%100==0):
-			#	torch.save(self.actor_model.state_dict(),'/home/nihar/Desktop/DeepRL/Curiosity/models/N_'+str(self.N)+'/actor/'+'actor_model_'+'N_'+str(self.N)+'_episode_'+str(e))
-			#	torch.save(self.critic_model.state_dict(),'/home/nihar/Desktop/DeepRL/Curiosity/models/N_'+str(self.N)+'/critic/'+'critic_model_'+'N_'+str(self.N)+'_episode_'+str(e))
-			
+			icm_optimizer.step()
+			np.save('mean_rewards_graph', mean_rewards)
+			if(e%10==0):
+				torch.save(self.inv_model.state_dict(),'./models/inv_model')
+				torch.save(self.icm_forward.state_dict(),'./models/icm_forward')
+				torch.save(self.icm_features.state_dict(),'./models/icm_features')
+				torch.save(self.features.state_dict(),'./models/A2C_features')
+				torch.save(self.actor_model.state_dict(),'./models/actor_model')
+				torch.save(self.critic_model.state_dict(),'./models/critic_model')
 		return
 
 	def ICMLoss(self, state, next_state, action_prob, lamda = 0.5, beta = 0.3):
@@ -128,18 +164,16 @@ def parse_arguments():
 						help="Path to the actor model config file.")
 	parser.add_argument('--num-episodes', dest='num_episodes', type=int,
 						default=50000, help="Number of episodes to train on.")
-	parser.add_argument('--lr', dest='actor_lr', type=float,
+	parser.add_argument('--icm-lr', dest='icm_lr', type=float,
+						default=5e-4, help="The actor's learning rate.")
+	parser.add_argument('--actor-lr', dest='actor_lr', type=float,
 						default=5e-4, help="The actor's learning rate.")
 	parser.add_argument('--critic-lr', dest='critic_lr', type=float,
 						default=1e-4, help="The critic's learning rate.")
 	parser.add_argument('--n', dest='n', type=int,
 						default=100, help="The value of N in N-step A2C.")
-	parser.add_argument('--load_critic', dest='load_critic', type=bool,
-						default=False, help="Load the critic model")
-	parser.add_argument('--load_actor', dest='load_actor', type=bool,
-						default=False, help="Load the actor model")
-	parser.add_argument('--load_episode', dest='e', type=int,
-						default=23100, help="Load the actor model")
+	parser.add_argument('--load_models', dest='load_models', type=bool,
+						default=False, help="Load all models")
 	parser_group = parser.add_mutually_exclusive_group(required=False)
 	parser_group.add_argument('--render', dest='render',
 								action='store_true',
@@ -156,6 +190,7 @@ def main(args):
 	args = parse_arguments()
 	curiosity = Curiosity(args)
 	curiosity.train(args)
+	# curiosity.play(args)
 
 if __name__ == '__main__':
 	main(sys.argv)
